d55ebde170f57e2c25c920a0c0e5fd4d79977028
daviddlv007 <davidlanzadlv7@gmail.com>
2025-04-11 21:11:35 -0400

    Solución de inconsistencia en el flujo del despliegue

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarioss")
+@RequestMapping("/usuariosss")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
